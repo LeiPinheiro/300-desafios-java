3c50560423fae9fe8c62bc71cf4733519a486c97
LeiPinheiro <contatopinheirolei@gmail.com>
2025-02-09 18:07:16 -0300

    desafio_001 commit

---
+++ dificuldade-fácil/arrays&strings-001/desafio_001.java
@@ -0,0 +1,14 @@
+public class desafio_001 {
+    public static int somar() {
+        int x = 4352;
+        int y = 1234;
+
+        return x + y;
+    }
+
+    public static void main(String[] args) {
+        int resultado = somar();
+
+        System.out.println("\nA soma dos dois numeros é " + resultado);
+    }
+}
